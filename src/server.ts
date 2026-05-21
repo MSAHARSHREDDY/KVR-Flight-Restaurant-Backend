1ccadaa8ea75ea554635b6cbf4c683538d5aa730
MSAHARSHREDDY <saharshreddym59@gmail.com>
2026-05-21 06:38:01 +0530

    Fix Cloud Run port

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,12 @@ import app from "./app";
 
 import connectDB from "./config/db";
 
-const PORT = process.env.port || 9000;
+const PORT = Number(process.env.PORT) || 8080;
+
 
 connectDB();
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(
     `Server running on port ${PORT}`
   );
